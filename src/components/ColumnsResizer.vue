6543bf7bb7ccb66b2a5489619a15ad80aee80674
test <test@test.com>
2024-06-01 10:45:22 +0800

    update

---
+++ src/components/ColumnsResizer.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="vue-columns-resizable" style="position: relative;">
+    <template v-for="(col, key) in cols" :key="key">
+      <div class="columns-resize-bar" ref="rbar" @mousedown="startResize(col, key, $event)"></div>
+    </template>
+    <div class="overlay" v-if="resizeColumn" @mousemove.prevent="handleResize" @mouseup.prevent="resizeBarMouseUp">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["table", "th", "cols"],
+  data() {
+    return {
+      resizeColumn: null,
+    };
+  },
+  mounted() {
+
+    const table = this.table;
+    const resizeObserver = new ResizeObserver(entries => {
+      for (const entry of entries) {
+        document.documentElement.style.setProperty('--table-height', table.offsetHeight + 'px');
+      }
+    });
+    resizeObserver.observe(table);
+
+    window.addEventListener('resize', () => {
+      this.winResize();
+    });
+    this.winResize();
+
+  },
+  methods: {
+    winResize() {
+      this.$nextTick(() => { this.resize(); });
+    },
+    resize() {
+
+      for (let i = 0; i < this.$refs.rbar.length; i++) {
+        this.$refs.rbar[i].style.left = this.th[i].offsetLeft + this.th[i].offsetWidth - this.$refs.rbar[i].offsetWidth / 2 + 'px';
+      }
+    },
+    winResize() {
+      this.$nextTick(() => { this.resize(); });
+    },
+
+    handleResize(event) {
+      if (this.resizeColumn) {
+        let i = this.resizeColumnIndex;
+        let width = this.resizeColumnWidth + event.x - this.resizeX;
+        this.resizeColumn.width = width;
+        let j = i;
+        this.$refs.rbar[i].style.left = this.th[j].offsetLeft + width - this.$refs.rbar[i].offsetWidth / 2 + 'px';
+      }
+
+    },
+    startResize(col, colIndex, event) {
+      this.resizeColumn = col;
+      this.resizeColumnIndex = colIndex;
+      this.resizeX = event.x;
+      this.resizeColumnWidth = this.th[colIndex].offsetWidth;
+      this.resizeLastColumnWidth = this.th[this.th.length - 1].offsetWidth;
+
+    },
+    resizeBarMouseUp() {
+      if (this.resizeColumn) {
+        this.resize();
+      }
+      this.resizeColumn = 0;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.resizable {
+  position: relative;
+  overflow: hidden;
+}
+
+.resize-handle {
+  position: absolute;
+  top: 0;
+  right: -11px;
+  bottom: 0;
+  width: 3px;
+  background-color: #eee;
+  cursor: col-resize;
+}
+
+.overlay {
+  position: fixed;
+  inset: 0;
+  z-index: var(--vt-index-overlay);
+  cursor: col-resize;
+  user-select: none;
+}
+
+.columns-resize-bar {
+  z-index: var(--vt-index-overlay);
+  position: absolute;
+  top: 0px;
+  width: 4px;
+  cursor: col-resize;
+  height: var(--table-height);
+}
+
+.columns-resize-bar:hover {
+  background-color: gray;
+}
+</style>
