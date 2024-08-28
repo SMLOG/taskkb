3b8f4e1ae00ae12d642869daf2c62440a94b070b
test <test@test.com>
2024-08-28 11:07:25 +0800

    fine tunning

---
+++ src/components/ColSeq.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="row">
+    <a class="etype num" :draggable="1">{{ row._rIndex }}</a>
+  </div>
+  <div v-else>
+    #
+  </div>
+</template>
+
+<script setup>
+</script>
+<script>
+export default {
+
+  props: {
+    col: {
+      type: Object
+    },
+    row: {
+      type: Object,
+      required: false,
+    },
+  },
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style></style>
