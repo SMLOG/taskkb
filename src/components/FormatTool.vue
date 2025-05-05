8e5468bf40bd006da8c01a46a7c1408c8f1a4b76
Your Name <you@example.com>
2025-05-05 03:16:01 +0000

    composition options

--- src/components/FormatTool.vue
+++ src/components/FormatTool.vue
@@ -1,136 +1,124 @@
 <template>
   <div @mouseup="checkSelection">
-  <slot ></slot>
-</div>
-  <div id="formatTool" v-show="isFormatToolVisible " :style="{ left: formatToolLeft, top: formatToolTop }">
-      <button @click="applyBold">{{isBoldNow?"Un":""}}Bold</button>
-      <button class="indicative-element" ref="indicativeElement" @click.prevent.stop="toggleColorSelect" style="color:">
+    <slot></slot>
+  </div>
+  <div id="formatTool" v-show="isFormatToolVisible" :style="{ left: formatToolLeft, top: formatToolTop }">
+    <button @click="applyBold">{{ isBoldNow ? 'Un' : '' }}Bold</button>
+    <button class="indicative-element" ref="indicativeElement" @click.prevent.stop="toggleColorSelect" style="color:">
       Color
-      </button><button @click="removeFontColor">-Color</button>
-      <div>
+    </button>
+    <button @click="removeFontColor">-Color</button>
+    <div>
       <ColorSelector v-model="fontColor" @select="selectColor" style="user-select: none;" v-if="showColorSelect" :position="colorSelectPosition"></ColorSelector>
-      </div>
-      
     </div>
+  </div>
 </template>
 
-<script>
-import ColorSelector from "./ColorSelector.vue";
+<script setup>
+import { ref, computed } from 'vue';
+import ColorSelector from './ColorSelector.vue';
 
-export default {
-  components: {
-    ColorSelector
+const props = defineProps({
+  editable: {
+    type: Boolean,
+    required: false,
   },
-  props: {
-    editable: {
-      type: Boolean,
-      required: false,
-    }
-  },
-  data() {
-    return {
-      isFormatToolVisible: false,
-      formatToolLeft: 0,
-      formatToolTop: 0,
-      fontColor: "#FF0000",
-      showColorSelect: false,
-      isBoldNow:false,
-      colorSelectPosition: {
-        top: 0,
-        left: 0
-      }
-    };
-  },
-  methods: {
-    isBold() {
-      const selectedText = window.getSelection().toString();
+});
 
-      if (selectedText.length > 0) {
-        const range = window.getSelection().getRangeAt(0);
-        const parentElement = range.commonAncestorContainer.parentElement;
-        const computedStyle = window.getComputedStyle(parentElement);
-        return computedStyle.fontWeight === "bold" || parseInt(computedStyle.fontWeight) >= 700;
-      }
+const isFormatToolVisible = ref(false);
+const formatToolLeft = ref(0);
+const formatToolTop = ref(0);
+const fontColor = ref('#FF0000');
+const showColorSelect = ref(false);
+const isBoldNow = ref(false);
+const colorSelectPosition = ref({
+  top: 0,
+  left: 0,
+});
+const indicativeElement = ref(null);
 
-      return false;
-    },
-    selectColor(color){
-      this.applyFontColor(null,color)
-      this.showColorSelect=false;
-    },
-    toggleColorSelect() {
-      this.showColorSelect = !this.showColorSelect;
-      if (this.showColorSelect) {
-        this.calculateColorSelectPosition();
-      }
-    },
-    calculateColorSelectPosition() {
-      const indicativeElementRect = this.$refs.indicativeElement.getBoundingClientRect();
-      this.colorSelectPosition = {
-        top: indicativeElementRect.bottom + 'px',
-        left: indicativeElementRect.left + 'px'
-      };
-    },
-    applyFontColor(event,color) {
-      console.log(color)
-      
-      if(color)this.fontColor=color;
-      document.execCommand("foreColor", false, this.fontColor);
-    },
-    removeFontColor() {
-      document.execCommand("removeFormat", false, "foreColor");
-    },
-    applyFontColor2() {
-      // Get the selected text
-      const selection = window.getSelection();
+const isBold = () => {
+  const selectedText = window.getSelection().toString();
 
-      // Check if the selected text is within a <span> element with the desired color
-      const parentSpan = selection.anchorNode.parentElement;
-      if (parentSpan.tagName === 'SPAN') {
-        parentSpan.style.color = this.fontColor;
-      } else {
-        // Create a new range based on the selected text
-        const range = selection.getRangeAt(0);
+  if (selectedText.length > 0) {
+    const range = window.getSelection().getRangeAt(0);
+    const parentElement = range.commonAncestorContainer.parentElement;
+    const computedStyle = window.getComputedStyle(parentElement);
+    return computedStyle.fontWeight === 'bold' || parseInt(computedStyle.fontWeight) >= 700;
+  }
 
-        // Create a <span> element to wrap the selected text
-        const span = document.createElement('span');
-        span.style.color = this.fontColor;
+  return false;
+};
 
-        // Surround the selected text with the <span> element
-        range.surroundContents(span);
-      }
-      console.log('apply color')
-    },
-    checkSelection() {
-      const selectedText = window.getSelection().toString();
-      // const container = this.$refs.container;
-      console.log('checkSelection')
+const selectColor = (color) => {
+  applyFontColor(null, color);
+  showColorSelect.value = false;
+};
 
-      if (selectedText.length > 0) {
-        this.isFormatToolVisible = true;
-        const selection = window.getSelection().getRangeAt(0);
-        const boundingRect = selection.getBoundingClientRect();
-        this.formatToolLeft = boundingRect.right + "px";
-        this.formatToolTop = boundingRect.bottom + "px";
-        this.isBoldNow= this.isBold()
-      } else {
-        this.isFormatToolVisible = false;
+const toggleColorSelect = () => {
+  showColorSelect.value = !showColorSelect.value;
+  if (showColorSelect.value) {
+    calculateColorSelectPosition();
+  }
+};
 
-      }
-    }, applyBold(event) {
-      event.preventDefault();
+const calculateColorSelectPosition = () => {
+  const indicativeElementRect = indicativeElement.value.getBoundingClientRect();
+  colorSelectPosition.value = {
+    top: indicativeElementRect.bottom + 'px',
+    left: indicativeElementRect.left + 'px',
+  };
+};
 
-      if (this.isBold()) {
-        document.execCommand("bold");
-        this.isBoldNow=false;
-      } else {
-        document.execCommand("bold", false, null);
-        this.isBoldNow=true;
-      }
-    },
-  },
-  computed: {
+const applyFontColor = (event, color) => {
+  if (color) fontColor.value = color;
+  document.execCommand('foreColor', false, fontColor.value);
+};
 
+const removeFontColor = () => {
+  document.execCommand('removeFormat', false, 'foreColor');
+};
+
+const applyFontColor2 = () => {
+  const selection = window.getSelection();
+
+  const parentSpan = selection.anchorNode.parentElement;
+  if (parentSpan.tagName === 'SPAN') {
+    parentSpan.style.color = fontColor.value;
+  } else {
+    const range = selection.getRangeAt(0);
+    const span = document.createElement('span');
+    span.style.color = fontColor.value;
+    range.surroundContents(span);
+  }
+  console.log('apply color');
+};
+
+const checkSelection = () => {
+  const selectedText = window.getSelection().toString();
+  console.log('checkSelection');
+
+  if (selectedText.length > 0) {
+    isFormatToolVisible.value = true;
+    const selection = window.getSelection().getRangeAt(0);
+    const boundingRect = selection.getBoundingClientRect();
+    formatToolLeft.value = boundingRect.right + 'px';
+    formatToolTop.value = boundingRect.bottom + 'px';
+    isBoldNow.value = isBold();
+  } else {
+    isFormatToolVisible.value = false;
+  }
+};
+
+const applyBold = (event) => {
+  event.preventDefault();
+
+  if (isBold()) {
+    document.execCommand('bold');
+    isBoldNow.value = false;
+  } else {
+    document.execCommand('bold', false, null);
+    isBoldNow.value = true;
   }
 };
 </script>
