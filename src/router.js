ff3cb39212b8ce8cc27d4d03595e2a5fdaf568ad
Your Name <you@example.com>
2025-05-16 14:09:39 +0000

    update

--- src/router.js
+++ src/router.js
@@ -3,13 +3,12 @@ import { createRouter,createWebHashHistory } from 'vue-router';
 import GridList from './components/GridList.vue';
 import TreeTimeline from './components/tree/TreeTimeline.vue';
 import Timeline from './components/Timeline.vue';
-import tree from './components/tree/grid.vue';
+//import tree from './components/tree/grid.vue';
 
 const routes = [
-  { path: '/', component: tree },
+  { path: '/', component: TreeTimeline },
   { path: '/grid', component: GridList },
   { path: '/timeline', component: Timeline },
-  { path: '/tree', component: TreeTimeline }
 ];
 
 const router = createRouter({
