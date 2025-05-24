61cee11ddd5af2c18f9d2724b81da1fbaef3eb02
Your Name <you@example.com>
2025-05-24 14:48:55 +0000

    update

---
+++ src/components/cpList.ts
@@ -0,0 +1,18 @@
+
+import { ref } from 'vue';
+import ColTitle from '@/components/ColTitle.vue';
+import ColDropText from '@/components/ColDropText.vue';
+import ColDate from '@/components/ColDate.vue';
+import ColSeq from '@/components/ColSeq.vue';
+const componentMap = {
+  ColTitle,
+  ColDropText,
+  ColDate,
+  ColSeq,
+};
+// Resolve component dynamically
+export function resolveComponent(cp) {
+  return componentMap[cp] || null; // Fallback to null if component not found
+};
+
+export const cpList = ref<string[]>(['ColTitle', 'ColDropText', 'ColDate', 'Time', 'ColSeq']);
