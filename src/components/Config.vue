ae8c85994293e6f3b406d2a0813962afcf7b7574
Your Name <you@example.com>
2025-05-24 02:04:40 +0000

    update

--- src/components/Config.vue
+++ src/components/Config.vue
@@ -4,7 +4,7 @@
   <!-- Popup Modal -->
   <div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center bg-gray-500 bg-opacity-10"
     @click.self="close()">
-    <button class="absolute right-4 top-4 text-gray-500 hover:text-gray-700" @click="close()">
+    <button class="absolute right-4 top-4 text-white hover:text-white" @click="close()">
       <svg class="h-6 w-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
       </svg>
