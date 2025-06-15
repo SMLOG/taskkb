42ff18cf726cee83315ebaa0ea82be5e87a09505
test <test@test.com>
2025-06-15 11:48:07 +0800

    update

--- src/components/Tabs.vue
+++ src/components/Tabs.vue
@@ -8,37 +8,16 @@
       @removeTab="showRmoveConfirmRef=index"
       @click="appStore.setActiveTab(index)"
     />
-    <button
-      @click="addTab"
-      class="plus-button text-gray-500 hover:text-blue-600 hover:bg-gray-200 dark:text-gray-400 dark:hover:text-blue-400 dark:hover:bg-gray-700 rounded-full transition-all duration-200"
-      title="Add New Tab"
-    >
-      <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
-      </svg>
-    </button>
+    <slot></slot>
+
   </div>
 
 </template>
 
 <script setup>
-import { ref } from 'vue';
 import { useAppStore } from "@/stores/appStore";
 import Tab from '@/components/Tab.vue';
-
-
-const showNewTabPopup = ref(false);
 const appStore = useAppStore();
-const addTab = () => {
-  showNewTabPopup.value = true;
-};
-
-const showRmoveConfirmRef = ref(-1);
-const confirmRemoveTab = ()=>{
-  appStore.removeTab(showRmoveConfirmRef.value);
-  showRmoveConfirmRef.value=-1;
-
-}
 
 </script>
 
--- src/components/TabsPanel.vue
+++ src/components/TabsPanel.vue
@@ -1,7 +1,15 @@
 <template>
-  <Tabs/>
+  <Tabs>
+    <button @click="addTab"
+      class="plus-button text-gray-500 hover:text-blue-600 hover:bg-gray-200 dark:text-gray-400 dark:hover:text-blue-400 dark:hover:bg-gray-700 rounded-full transition-all duration-200"
+      title="Add New Tab">
+      <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
+      </svg>
+    </button>
+  </Tabs>
   <NewTabPopup v-if="showNewTabPopup" v-model="showNewTabPopup" />
-  <ConfirmPopUp v-if="showRmoveConfirmRef>-1" @cancel="showRmoveConfirmRef=-1" @confirm="confirmRemoveTab()"/>
+  <ConfirmPopUp v-if="showRmoveConfirmRef > -1" @cancel="showRmoveConfirmRef = -1" @confirm="confirmRemoveTab()" />
 </template>
 
 <script setup>
@@ -11,21 +19,24 @@ import Tabs from '@/components/Tabs.vue';
 import NewTabPopup from '@/components/NewTabPopup.vue';
 import ConfirmPopUp from '@/components/ConfirmPopUp.vue';
 
-const showNewTabPopup = ref(false);
 const appStore = useAppStore();
 
+const showNewTabPopup = ref(false);
+
+const addTab = () => {
+  showNewTabPopup.value = true;
+};
 
 const showRmoveConfirmRef = ref(-1);
-const confirmRemoveTab = ()=>{
+const confirmRemoveTab = () => {
   appStore.removeTab(showRmoveConfirmRef.value);
-  showRmoveConfirmRef.value=-1;
+  showRmoveConfirmRef.value = -1;
 
 }
 
 </script>
 
 <style scoped>
-
 .plus-button {
   display: flex;
   align-items: center;
