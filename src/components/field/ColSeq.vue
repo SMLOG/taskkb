2d864a99f02eb0cee1a142057cd14ba399d018cd
Your Name <you@example.com>
2025-06-16 11:50:30 +0800

    update

--- src/components/field/ColSeq.vue
+++ src/components/field/ColSeq.vue
@@ -1,5 +1,5 @@
 <template>
-  <a v-if="row" class="etype num block" :draggable="isDrag">{{ index }}</a>
+  <a v-if="row" class="etype num block flex-1" :draggable="isDrag">{{ index }}</a>
   <div v-else>#</div>
 </template>
 
