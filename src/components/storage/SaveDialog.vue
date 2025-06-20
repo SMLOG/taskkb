9c0e4f9730ddbaf434ecd62500208612fe6bc1f5
Your Name <you@example.com>
2025-06-20 21:48:31 +0800

    update

--- src/components/storage/SaveDialog.vue
+++ src/components/storage/SaveDialog.vue
@@ -18,10 +18,7 @@
           id="where" 
           class="w-full p-2 border border-gray-300 dark:border-gray-600 rounded bg-white dark:bg-gray-700 text-gray-900 dark:text-gray-100 focus:ring-2 focus:ring-blue-500 dark:focus:ring-blue-400 outline-none transition-colors duration-200"
         >
-        <option value="google">Google Drive - My Drive</option>
-        <option value="googlefolder">Google Drive - Pick a folder...</option>
-        <option value="browser">Browser</option>
-          <option value="device">Device</option>
+        <option  v-for="option in modesRef" :value="option.mode">{{ option.name }}</option>
         </select>
       </div>
 
@@ -51,6 +48,13 @@ import { storeToRefs } from 'pinia';
 
 const {fileName,showAuth,mode} = storeToRefs(useModeStore());
 const emit = defineEmits(['saved']);
+
+const modesRef = ref([
+{mode:'G',name:"Google Drive - My Drive"}
+,{mode:'G',name:"Google Drive - Pick a folder...",folder:true}
+,{mode:'L',name:"Browser"}
+,{mode:'D',name:"Device"}
+])
 
 const storageLocation = ref('google');
 const isOpen = ref(false);
