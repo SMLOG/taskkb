295f26f10559537f7c272d4c9e63eeee1599f886
Your Name <you@example.com>
2025-06-26 10:05:16 +0800

    update

--- src/api/bridge.js
+++ src/api/bridge.js
@@ -1,25 +1,5 @@
 export async function getStorageBridge(m) {
-    const isForgeEnv = typeof window !== 'undefined' && [
-        atob('cGVyZmVjdHRvZG8uY29t'),
-        atob('dHJlZWdyaWQuaW8')
-    ].filter(domain => location.href.indexOf(domain) > -1).length == 0;
-    let mode = m || "L";
-
-    try {
-        if (isForgeEnv) {
-            mode = "J"
-        }
-
-        return getStorageBridgeByName(mode);
-
-
-    } catch (error) {
-        console.error('Error checking Forge environment:', error);
-    }
-
-    const { readJsonAttachment, writeObjectToJsonAttachment, type } = await import('@/api/google');
-    return { readJsonAttachment, writeObjectToJsonAttachment, type };
-
+    return getStorageBridgeByName(m);
 }
 
 export async function getStorageBridgeByName(mode) {
