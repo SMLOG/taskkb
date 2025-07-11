d48df6312e233af72ff6c6efa27c2fe8afd1eed3
Your Name <you@example.com>
2025-07-11 12:09:06 +0800

    update

--- src/components/tree/TreeCard.vue
+++ src/components/tree/TreeCard.vue
@@ -1,13 +1,14 @@
 <template>
-      <!-- Main Card -->
-      <div v-if="depth !== ''" class="card min-h-[70px] min-w-[200px] max-w-[500px] bg-white rounded-lg shadow-sm border border-gray-200 overflow-hidden" >
-        <template v-for="(col, cellIndex) in cols" :key="cellIndex">
-
-            <div class="flex">
-            <div class="cell flex px-1 p-2">
-              <b class="w-full"><component :is="resolveComponent(col.cp)" :col="col" v-if="resolveComponent(col.cp)"></component></b>
-            </div>
-
+    <!-- Main Card -->
+    <div v-if="depth !== ''" class="card min-h-[70px] min-w-[200px] max-w-[500px] bg-white dark:bg-gray-800 rounded-lg shadow-sm border border-gray-200 dark:border-gray-700 overflow-hidden">
+      <template v-for="(col, cellIndex) in cols" :key="cellIndex">
+        <div class="flex">
+          <div class="cell flex px-1 p-2">
+            <b class="w-full text-gray-900 dark:text-gray-100">
+              <component :is="resolveComponent(col.cp)" :col="col" v-if="resolveComponent(col.cp)"></component>
+            </b>
+          </div>
+  
           <Cell 
             :row="row" 
             :col="col" 
@@ -15,29 +16,29 @@
             :cellIndex="cellIndex" 
             :index="id" 
             :depth="depth"
-            class="p-2 border-r border-gray-100 last:border-r-0 flex-1"
+            class="p-2 border-r border-gray-100 dark:border-gray-700 last:border-r-0 flex-1"
           ></Cell>
         </div>
-        </template>
-      </div>
-  
-      <!-- Child Cards -->
-      <template v-if="row._childs?.length && !row._collapsed" class="ml-6 pl-2 border-l-2 border-gray-200">
-        <TreeCard 
-          v-for="(child, index) in row._childs" 
-          :key="index" 
-          :depth="depth + '.' + index" 
-          :row="child"
-          :cols="cols" 
-          :showSch="showSch" 
-          :days="days" 
-          :firstDay="firstDay" 
-          :level="level + 1" 
-          :id="(id ? id + '.' : '') + (index + 1)" 
-          :gridStyle="gridStyle" 
-          :weeks="weeksRef" 
-        />
       </template>
+    </div>
+  
+    <!-- Child Cards -->
+    <template v-if="row._childs?.length && !row._collapsed" class="ml-6 pl-2 border-l-2 border-gray-200 dark:border-gray-700">
+      <TreeCard 
+        v-for="(child, index) in row._childs" 
+        :key="index" 
+        :depth="depth + '.' + index" 
+        :row="child"
+        :cols="cols" 
+        :showSch="showSch" 
+        :days="days" 
+        :firstDay="firstDay" 
+        :level="level + 1" 
+        :id="(id ? id + '.' : '') + (index + 1)" 
+        :gridStyle="gridStyle" 
+        :weeks="weeksRef" 
+      />
+    </template>
   </template>
   
   <script setup>
@@ -46,7 +47,7 @@
   import TreeCard from '@/components/tree/TreeCard.vue';
   import Cell from '@/components/tree/Cell.vue';
   import { resolveComponent } from '@/components/cpList';
-
+  
   const { weeksRef } = useTree();
   
   const props = defineProps({
@@ -69,6 +70,4 @@
     display: grid;
     grid-template-columns: repeat(var(--cols-count, 1), minmax(0, 1fr));
   }
-  
-  /* You can keep your existing tree.css or replace with these Tailwind classes */
   </style>
