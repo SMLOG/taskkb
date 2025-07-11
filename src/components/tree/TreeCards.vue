4260f44060dc53779b9bbd86bf47c34b99b8a561
Your Name <you@example.com>
2025-07-11 21:49:48 +0800

    update

--- src/components/tree/TreeCards.vue
+++ src/components/tree/TreeCards.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative min-w-full">
-    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 p-4"
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 p-4"
     >
       <TreeCard
         :row="treeRef"
