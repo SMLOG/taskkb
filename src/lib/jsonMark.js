82e7d52d8c1ee83bebabb91f4e9a0fb0e2b9803f
test <test@test.com>
2025-07-20 08:39:09 +0800

    upadte

--- src/lib/jsonMark.js
+++ src/lib/jsonMark.js
@@ -1,20 +1,28 @@
 export function jsonToMarkdown(json, level = 0) {
     let markdown = '';
-    const indent = '  '.repeat(level);
+    const indent = '  '.repeat(Math.max(level-1,0));
     
     if (typeof json === 'object' && json !== null) {
+      if( level>0)
       for (const [key, value] of Object.entries(json)) {
         if (typeof value === 'object' && value !== null) {
+          if(key==='rows')continue;
           markdown += `${indent}- **${key}**:\n${jsonToMarkdown(value, level + 1)}`;
         } else {
           markdown += `${indent}- **${key}**: ${value}\n`;
         }
       }
-    } else {
-      markdown += `${indent}- ${json}\n`;
+      if( json.rows instanceof Array)
+        for(const value of json.rows) {
+          if (typeof value === 'object' && value !== null) {
+             markdown += '\n\n'+jsonToMarkdown(value, level + 1)
+          }
+        }
+      
+
     }
     
-    return markdown;
+    return markdown.trim();
   }
   
  export function markdownToJson(markdown) {
