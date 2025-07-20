092f1840890a90845f57028273829045339bdc61
test <test@test.com>
2025-07-20 08:12:34 +0800

    upadte

--- src/components/tree/SourceCode.vue
+++ src/components/tree/SourceCode.vue
@@ -8,12 +8,23 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import MonacoEditor from './MonacoEditor.vue'
+import {jsonToMarkdown} from '@/lib/jsonMark';
+import { storeToRefs } from 'pinia';
+import { useAppStore } from '@/stores/appStore';
+const appStore = useAppStore();
+const { treeRef} = storeToRefs(appStore);
 
 const code = ref('// TypeScript example\ninterface User {\n  name: string\n  age: number\n}')
 
 const handleChange = (value) => {
   console.log('Editor content changed:', value)
 }
+
+watch(treeRef, (newTree) => {
+  if (newTree) {
+   code.value = jsonToMarkdown(newTree)
+  }
+}, { immediate: true })
 </script>
