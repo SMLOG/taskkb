56204b5d3d097c026eebf4391c16393f04bdb9cf
Your Name <you@example.com>
2025-08-06 14:53:14 +0800

    update

--- src/components/field/ColSeq.vue
+++ src/components/field/ColSeq.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="row" class="etype num block flex-1" :draggable="isDrag">{{ index }}</div>
+  <div v-if="row" class="etype num block flex-1" :draggable="isDrag"><span :class="{major:index.length==1}">{{ index }}</span></div>
   <div v-else>#</div>
 </template>
 
@@ -25,5 +25,15 @@ defineProps({
 <style>
 .selected .num{
   cursor: move;
+}
+.major{
+    background: darkblue;
+    display: inline-block;
+    border-radius: 100%;
+    text-align: center;
+    line-height: 1em;
+    line-height: 1.2em;
+    width: 1.2em;
+    color: white;
 }
 </style>
